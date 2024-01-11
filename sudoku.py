d34c64b6ef026ebdfbe4dfeab0f03b1188fb2d51
Adelina Javgureanu <126191298+adelinajav@users.noreply.github.com>
2024-01-11 13:54:30 -0500

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,64 @@
+def empty(puzzle):
+    for x in range(9):
+        for y in range(9):
+            if puzzle[x][y] ==-1:
+                return x, y
+    return None, None 
+
+def is_valid(puzzle, guess, row, col):
+
+    rowval = puzzle[row]
+    if guess in rowval:
+        return False
+    
+    colval = []
+    for i in range(9):
+        colval.append(puzzle[i][col])
+    if guess in colval:
+        return False
+    
+
+    rowstart = (row//3) *3
+    colstart = (col//3) *3
+
+    for r in range(rowstart, rowstart+3):
+        for c in range(colstart, colstart+3):
+            if puzzle[r][c] == guess:
+                return False
+
+    return True
+
+
+
+def solver(puzzle):
+    row, col = empty(puzzle)
+
+    if row is None:
+        return True
+    for guess in range(1, 10):
+        if is_valid(puzzle, guess, row, col):
+            puzzle[row][col] = guess
+            if solver(puzzle):
+                return True
+        puzzle[row][col] = -1
+    
+    return False
+
+
+//example:
+example = [
+    [5, 3, -1, -1, 7, -1, -1, -1, -1],
+    [6, -1, -1, 1, 9, 5, -1, -1, -1],
+    [-1, 9, 8, -1, -1, -1, -1, 6, -1],
+    [8, -1, -1, -1, 6, -1, -1, -1, 3],
+    [4, -1, -1, 8, -1, 3, -1, -1, 1],
+    [7, -1, -1, -1, 2, -1, -1, -1, 6],
+    [-1, 6, -1, -1, -1, -1, 2, 8, -1],
+    [-1, -1, -1, 4, 1, 9, -1, -1, 5],
+    [-1, -1, -1, -1, 8, -1, -1, 7, 9]
+]
+
+solver(example)
+for row in example:
+    print(row)
+    
